e62a9b7eb712c697c6bbfd7996a7770c8eafe56b
FeSanz <68288843+FeSanz@users.noreply.github.com>
2023-10-09 18:18:38 -0600

    Primera Subida
    OK

---
+++ WOW P2/API.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WOW_P2
+{
+    internal class API
+    {
+        private WebRequest request;
+        private WebResponse response;
+        private StreamReader reader;
+        private StreamWriter writer;
+        private Stream stream;
+
+        private string url = "https://iapxqy-test.fa.ocs.oraclecloud.com/fscmRestApi/resources/11.13.18.05";
+        private string _user = "felipe.antonio@i-condor.com";
+        private string _password = "CondorXR112";
+
+        public async Task<string> GetRequestAsync(string path)
+        {
+            string credential = Convert.ToBase64String(Encoding.GetEncoding("ISO-8859-1").GetBytes(_user + ":" + _password));
+            try
+            {
+                request = WebRequest.Create(url + path);
+                request.Headers.Add("Authorization", "Basic " + credential);
+                response = await request.GetResponseAsync();
+                stream = response.GetResponseStream();
+                reader = new StreamReader(stream);
+                return await reader.ReadToEndAsync();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error GET. " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return null;
+            }
+        }
+        public async Task<string> PostRequestAsync(string path, string json)
+        {
+            string credential = Convert.ToBase64String(Encoding.GetEncoding("ISO-8859-1").GetBytes(_user + ":" + _password));
+            try
+            {
+                request = WebRequest.Create(url + path);
+                request.Headers.Add("Authorization", "Basic " + credential);
+                request.ContentType = "application/json";
+                request.Method = "POST";
+
+                writer = new StreamWriter(request.GetRequestStream());
+                writer.Write(json);
+                writer.Flush();
+                writer.Close();
+
+                response = await request.GetResponseAsync();
+                reader = new StreamReader(response.GetResponseStream());
+                return await reader.ReadToEndAsync();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error POST. " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return null;
+            }
+        }
+
+        public string GetRequest(string path)
+        {
+            string credential = Convert.ToBase64String(Encoding.GetEncoding("ISO-8859-1").GetBytes(_user + ":" + _password));
+            try
+            {
+                request = WebRequest.Create(url + path);
+                request.Headers.Add("Authorization", "Basic " + credential);
+                response = request.GetResponse();
+                stream = response.GetResponseStream();
+                reader = new StreamReader(stream);
+                return reader.ReadToEnd();
+            }
+            catch (WebException ex)
+            {
+                MessageBox.Show("Error GET. " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return null;
+            }
+        }
+
+        public string PostRequest(string path, string json)
+        {
+            string credential = Convert.ToBase64String(Encoding.GetEncoding("ISO-8859-1").GetBytes(_user + ":" + _password));
+            try
+            {
+                request = WebRequest.Create(url + path);
+                request.Headers.Add("Authorization", "Basic " + credential);
+                request.ContentType = "application/json";
+                request.Method = "POST";
+
+                writer = new StreamWriter(request.GetRequestStream());
+                writer.Write(json);
+                writer.Flush();
+                writer.Close();
+
+                response = request.GetResponse();
+                reader = new StreamReader(response.GetResponseStream());
+                return reader.ReadToEnd();
+            }
+            catch (WebException ex)
+            {
+                MessageBox.Show("Error POST. " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return null;
+            }
+        }
+    }
+}
---
+++ WOW P2/frmLabels.cs
@@ -0,0 +1,181 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Tulpep.NotificationWindow;
+using WOW_P2.Properties;
+
+namespace WOW_P2
+{
+    public partial class MainP2 : Form
+    {
+        PopupNotifier pop = new PopupNotifier();
+        API api;
+
+        public static string pylOrganization = string.Empty;
+        public static string organizationId = "300000002650049";
+
+        
+
+        public MainP2()
+        {
+            InitializeComponent();
+        }
+
+        private void MainP2_Load(object sender, EventArgs e)
+        {
+            lblVersion.Text = "v " + Assembly.GetExecutingAssembly().GetName().Version.ToString();
+            api = new API();
+
+            btnGetWeight.Text = "TARA";
+
+            RequestOrganizationData();
+            RequestWorkOrdersList();
+        }
+
+        private void LoadResourcesInit()
+        {
+            
+        }
+
+        private async void RequestOrganizationData()
+        {
+            try
+            {
+                Task<string> tskOrganizationData = api.GetRequestAsync("/inventoryOrganizations?fields=OrganizationId,OrganizationCode,OrganizationName,LocationCode&onlyData=true" +
+                                                                     "&limit=500&totalResults=true&q=OrganizationId=" + organizationId);
+                string response = await tskOrganizationData;
+                if (!string.IsNullOrEmpty(response))
+                {
+                    pylOrganization = response;
+                    var doOrganizationData = JsonConvert.DeserializeObject<dynamic>(response);
+                    lblLocationCode.Text = (string)doOrganizationData["items"][0]["LocationCode"];
+                }
+                else
+                {
+                    PopupNotification("Sin respuesta", null);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error. " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private async void RequestWorkOrdersList()
+        {
+            try
+            {
+                Task<string> tskWorkOrdersList = api.GetRequestAsync("/workOrders?limit=500&totalResults=true&onlyData=true&fields=WorkOrderNumber&q=OrganizationId=" + organizationId);
+                string response = await tskWorkOrdersList;
+                if (!string.IsNullOrEmpty(response))
+                {
+                    var doWorkOrderList = JsonConvert.DeserializeObject<dynamic>(response);
+
+                    int itemCount = (int)doWorkOrderList["count"];
+
+                    if (itemCount >= 1)
+                    {
+                        cmbWorkOrders.Items.Clear();
+
+                        for (int i = 0; i < itemCount; i++)
+                        {
+                            cmbWorkOrders.Items.Add((string)doWorkOrderList["items"][i]["WorkOrderNumber"]);
+                        }
+                    }
+                    else
+                    {
+                        PopupNotification("Sin ordenes de trabajo", null);
+                    }
+                }
+                else
+                {
+                    PopupNotification("Sin respuesta", null);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error. " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private async void SelectedIndexChangedWorkOrders(object sender, EventArgs e)
+        {
+            try
+            {
+                Task<string> tskWorkOrdersData = api.GetRequestAsync("/workOrders?q=OrganizationCode=" + lblLocationCode.Text + ";WorkOrderStatusCode=RELEASED&limit=500&totalResults=true&onlyData=true");
+                string response = await tskWorkOrdersData;
+                if (!string.IsNullOrEmpty(response))
+                {
+                    var doWorkOrderData = JsonConvert.DeserializeObject<dynamic>(response);
+
+                    lblPlannedQuantity.Text = (string)doWorkOrderData["items"][0]["PlannedStartQuantity"];
+                    lblCompletedQuantity.Text = (string)doWorkOrderData["items"][0]["CompletedQuantity"];
+                    
+                    lblOperation.Text = (string)doWorkOrderData["items"][0]["PlannedStartDate"];
+
+                    lblPlannedStartDate.Text = (string)doWorkOrderData["items"][0]["PlannedStartDate"];
+                    lblPlannedCompletionDate.Text = (string)doWorkOrderData["items"][0]["PlannedCompletionDate"];
+
+                    lblMachineCode.Text = (string)doWorkOrderData["items"][0]["PlannedStartDate"];
+                    lblMachineName.Text = (string)doWorkOrderData["items"][0]["PlannedStartDate"];
+
+                    lblItemNumber.Text = (string)doWorkOrderData["items"][0]["ItemNumber"];
+                    lblItemDescription.Text = (string)doWorkOrderData["items"][0]["Description"];
+                    lblUoM.Text = (string)doWorkOrderData["items"][0]["UnitOfMeasure"];
+                }
+                else
+                {
+                    PopupNotification("No se encontraron datos", null);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error. " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+        
+        private void PopupNotification(string content, Image icon)
+        {
+            pop.ContentText = content;
+            pop.ContentColor = Color.Black;
+            pop.ContentFont = new Font("Arial", 14);
+            pop.ContentPadding = new Padding(10, 15, 10, 5);
+            pop.ShowGrip = false;
+            pop.HeaderHeight = 1;
+            pop.BorderColor = Color.DarkGray; //Color.FromArgb(35, 35, 35);
+            pop.Image = icon;
+            pop.ImageSize = new Size(70, 70);
+            pop.ImagePadding = new Padding(10);
+            pop.Size = new Size(350, 90);
+            pop.IsRightToLeft = false;
+            pop.Popup();
+        }
+
+        private void btnGetWeight_Click(object sender, EventArgs e)
+        {
+            btnGetWeight.Text = "OBTENER";
+            if (string.IsNullOrEmpty(lblTareWeight.Text))
+            {
+                //Solicitar peso de tara a bascula
+            }
+            else
+            {
+                //Solictar peso de rollos
+            }
+        }
+
+        private void GetWeight()
+        {
+
+        }
+    }
+}
